9b6ee9c6ff813f9e16dfab5196bb8eaed1cd46e3
sdxdxx007 <1981623651@qq.com>
2025-10-07 02:35:58 -0400

    清空

--- Assets/Shader学习整理/03_PostProcessing/NormalLine/NormalLineVolume.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
-
-public class NormalLineVolume : VolumeComponent
-{
-    public  ColorParameter ColorChange = new ColorParameter(Color.white, true);
-}
--- Assets/Shader学习整理/03_PostProcessing/Outline/OutlineVolume.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
-
-public class OutlineVolume : VolumeComponent
-{
-    public  ColorParameter OutlineColor = new ColorParameter(Color.white, true);
-    public FloatParameter Rate = new FloatParameter(0);
-}
